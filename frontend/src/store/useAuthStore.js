0e02ec3e07f06e08fecfd40eabd08a1ff36e40c7
salartalaa <salarmhmdn@gmail.com>
2025-09-14 14:22:40 +0330

    sidebar completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -56,7 +56,18 @@ export const useAuthStore = create((set) => ({
       toast.success("Logged out successfully");
     } catch (error) {
       toast.error(error.response.data.message);
-      console.log('Logout error: ', error)
+      console.log("Logout error: ", error);
+    }
+  },
+
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.message);
     }
   },
 }));
