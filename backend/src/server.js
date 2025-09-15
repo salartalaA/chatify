cf782ab0fcf0ff69b1c837720610d6610813eb28
salartalaa <salarmhmdn@gmail.com>
2025-09-15 12:00:53 +0330

    fix server.js file issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
@@ -9,11 +10,12 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "10mb" })); // req.body
+app.use(express.json({ limit: "10mb" }));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
@@ -22,14 +24,14 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname), "../frontend/dist"));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("Server connect on port", PORT);
+  console.log("✅ Server connected on port", PORT);
   connectDB();
 });
